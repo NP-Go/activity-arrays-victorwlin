752e92666cc25aad4bfedbd3be4ea949adcf5b7b
Victor Lin <linvictorw@gmail.com>
2022-03-29 17:10:58 +0800

    Create and fill array

--- arrays.go
+++ arrays.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	//Insert Code here
+	var a [7]string
+	a[0] = "Operating System List"
+	a[1] = "Windows XP"
+	a[2] = "Linux 1.0"
+	a[3] = "Raspbian Teensy"
+	a[4] = "MacOS"
+	a[5] = "iOS"
+	a[6] = "Google Android"
+
+	for _, v := range a {
+		fmt.Println(len(v))
+	}
 }
